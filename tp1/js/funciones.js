633daa35f9d01a19c221b2929b44dc44c267748e
Felipe Franco <120602732+Felipe-258@users.noreply.github.com>
2023-04-14 19:13:26 -0300

    TP1

---
+++ tp1/js/funciones.js
@@ -0,0 +1,6 @@
+function mifuncion(){
+    const apellido=document.getElementById("name");
+    const nombre=document.getElementById("surname");
+    const edad=document.getElementById("age");
+    alert("Apellido: "+apellido.value+"\nNombre: "+nombre.value+"\nEdad: "+edad.value)
+}
